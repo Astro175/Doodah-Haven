5f1e4cbbe08932c1491737b879ae5fabc13d9608
astro175 <aspark400@gmail.com>
2023-10-10 19:27:26 +0100

    added extra validator for email and password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,16 +1,27 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 // Registers a user
 const registerUser = async (req, res) => {
-    const { firstname, lastname, email, password, role } = req.body
+    const { firstname, lastname, email, password, role } = req.body;
 
+    //Using validator for password and email validation
+    
     if (!email || !password) {
-        return res.status(200).json({error: "input all credential fields"});
+        return res.status(400).json({error: "input all credential fields"});
+    }
+
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({error: "Email is not valid"});
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        return res.status(400).json({error: "Password is not strong enough"});
     }
     const exists = await User.findOne({ email });
     if (exists) {
-         return res.status(200).json({error: "User already exists"});
+         return res.status(400).json({error: "User already exists"});
     }
     const hashedpw = await bcrypt.hash(password, 10)
     const user = await User.create({ firstname, lastname, email, password:hashedpw, role });
