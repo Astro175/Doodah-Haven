a4b486d2778f27339c61af66f40f26c7d10ad9f9
astro175 <aspark400@gmail.com>
2023-10-10 13:59:03 +0100

    tested database connection and made a dummy entry for users

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authController');
+const User = require('../models/userModel');
 
 const router = express.Router()
 
+
+// Test route for creating a new user, checking atlas functionality
+
+// Posts a new user
+router.post('/', async (req, res) => {
+    const { firstname, lastname, email, password, role } = req.body
+
+    try {
+        const user = await User.create({ firstname, lastname, email, password, role });
+        res.status(200).json(user);
+    } catch(error) {
+        res.status(400).json({error: error.message})
+
+    }
+
+})
 //Signup route
 router.post('/register', registerUser);
 
