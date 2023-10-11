a701ebdcfeb7fe3082b304b7e2882f66dd8966b3
astro175 <aspark400@gmail.com>
2023-10-11 13:53:30 +0100

    tweaked token.js middleware to use auth bearer tokens

--- server/middleware/token.js
+++ server/middleware/token.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '../.env' });
-const secret_key = process.env.SECRET_KEY
+const secret_key = process.env.SECRET_KEY;
+const User = require('../models/userModel');
 
-const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization; // Although this is saved in our db and sent to the client
-                                           // I think it should be set in the client auth header or session or as a cookie
-                                           // So that we can make use of the expression above
-
-  if (!token) return res.status(401).json({ error: 'Unauthorized' });
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization; // Although this is saved in our db and sent to the client
+                                           // It should be sent in the authorization header as a bearer token
+  if (!authorization) return res.status(401).json({ error: 'Authorization token required' });
+  const token = authorization.split(' ')[1] 
+  
 
   try {
-    const decoded = jwt.verify(token, secret_key);
-    req.user = decoded;
+    const tokenId = jwt.verify(token, secret_key);
+    req.user = await User.findOne({ tokenId }).select('_id');
   } catch (error) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
