764bf6396ddd85e35f4ed0358c05f3ecf12ca902
Nnenna Udefi <miriamudefi@gmail.com>
2023-10-17 22:13:03 +0100

    handled shipment, delivery and payment pge

--- client/src/components/payment/Payment.js
+++ client/src/components/payment/Payment.js
@@ -4,13 +4,30 @@ import { faArrowLeft, faTrash, faCheckCircle} from '@fortawesome/free-solid-svg-
 import { Link } from 'react-router-dom'
 import { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../context/CartContext';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css'
 
 const Payment = () => {
     const { cart, removeFromCart } = useContext(CartContext);
     const [cartItems, setCartItems] = useState([]);
     const [activeLink, setActiveLink] = useState('Payment'); // State to track the active link
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [selectedState, setSelectedState] = useState('');
 
 
+    const handleStateChange = (event) => {
+        setSelectedState(event.target.value);
+    }
+
+    const states = ['', 'Lagos', 'Abuja', 'Kano', 'Ogun', 'Oyo', 'Enugu', 'Kwara', 'Imo', 'Anambra', 'Delta',
+    'Cross-River', 'Akwa-Ibom', 'Rivers', 'Bayelsa', 'Kaduna', 'Abuja', 'Jos', 'Adamawa', 'Ebonyi', 'Abia']
+    const handlePhoneNumberChange = (value) => {
+        setPhoneNumber(value);
+    }
+
+    const handleClick = (link) => {
+        setActiveLink(link);
+    }
    
     useEffect(() => {
         setCartItems(cart)
@@ -21,7 +38,7 @@ const Payment = () => {
     };
 
     const calculateSalesTax = () => {
-        return calculateTotalPrice() * 0.065;
+        return calculateTotalPrice() * 0.02;
     };
       
     const totalDue = calculateTotalPrice() + calculateSalesTax();
@@ -64,7 +81,7 @@ const Payment = () => {
 
                     <p>Subtotal <span>N{calculateTotalPrice()}</span></p>
                     <hr />
-                    <p>Sales tax (6.5%) <span>N{calculateSalesTax()}</span></p>
+                    <p>Sales tax (2%) <span>N{calculateSalesTax()}</span></p>
                     <hr />
                     <p>Shipping fee <span className='free'>FREE</span></p>
                     <hr />
@@ -73,12 +90,7 @@ const Payment = () => {
             </div>
             <div className='pay-ship'>
             <div className='payment-link'>
-                <Link className={activeLink === 'Payment' ? 'active-link' : 'link'} onClick={() => setActiveLink('Payment')}>
-                Payment
-                </Link>
-                -- <FontAwesomeIcon icon={faCheckCircle} /> ---
-
-                <Link className={activeLink === 'Shipping' ? 'active-link' : 'link'} onClick={() => setActiveLink('Shipping')}>
+            <Link className={activeLink === 'Shipping' ? 'active-link' : 'link'} onClick={() => setActiveLink('Shipping')}>
                 Shipping
                 </Link>
                 -- <FontAwesomeIcon icon={faCheckCircle} /> ---
@@ -86,19 +98,109 @@ const Payment = () => {
                 <Link className={activeLink === 'Delivery' ? 'active-link' : 'link'} onClick={() => setActiveLink('Delivery')}>
                 Delivery
                 </Link>
-          </div>
+                -- <FontAwesomeIcon icon={faCheckCircle} /> ---
+
+                <Link className={activeLink === 'Payment' ? 'active-link' : 'link'} onClick={() => setActiveLink('Payment')}>
+                Payment
+                </Link>
+            </div>
 
                 <div className='toPay'>
+                    {activeLink === 'Shipping' && (
+                        <div>
+                            <form>
+                                <div className='shipment-form'>
+                                    <h2>Contact Details</h2>
+                                    <label htmlFor='firstname'>Firstname:</label><br />
+                                    <input type='text' name='firstname' />
+
+                                    <label htmlFor='lastname'>Lastname:</label>
+                                    <input type='text' name='lastname' /><br />
+
+                                    <label htmlFor='email'>Email:</label><br />
+                                    <input type='email' name='email' /><br />
+
+                                    <label htmlFor='number'>Phone number:</label><br />
+                                    <PhoneInput value={phoneNumber} defaultCountry='NG' onChange={handlePhoneNumberChange} id='number'/>
+                                </div>
+
+                                <div className='shipment-form'>
+                                    <h2>Shipping Details</h2>
+                                    <label htmlFor='addressNo'>Flat/House no.:</label><br />
+                                    <input type='number' name='addressNo' /><br />
+
+                                    <label htmlFor='address'>Address:</label><br />
+                                    <input type='text' name='address' /><br/>
+
+                                    <label htmlFor='city'>City:</label><br />
+                                    <input type='text' name='city' />
+
+                                    <label htmlFor='state'>State:</label><br />
+                                    {/* <input type='text' name='state' /> */}
+                                    <select id='state' name='state' value={selectedState} onChange={handleStateChange}>
+                                    {/* <option value='' disabled>Select a state</option> */}
+                                        {states.map((state, index) => {
+                                           return (
+                                            <option key={index} value={state}>
+                                                {state}
+                                            </option>
+                                           )
+                                        })}
+                                    </select>
+
+                                    <label htmlFor='code'>Postal Code:</label><br />
+                                    <input type='number' name='code' />
+
+                                    <label htmlFor='landmark'>Famous Landmark:</label><br />
+                                    <input type='text' name='landmark' /><br />
+
+                                    <input type='checkbox' name='check' />
+                                    <label htmlFor='check' className='checkbox'>My shipping and Billing address are the same</label>
+                                </div>
+                                <button type='submit' onClick={() => handleClick('Delivery')}>Continue to Delivery</button>
+                            </form>
+
+                            
+                        </div>
+                    )};
+
+                    {activeLink === 'Delivery' && (
+                            <div>
+                        <form className='payment-form'>
+                            <h2>Delivery Options</h2>
+                            <input type="radio" id="outsideLagos" name="outsideLagos" value="Outside lagos" />
+                            <label for="outsideLagos">Outside Lagos Home Delivery via DHL usually takes between 5 - 7 working days
+                                <p>Pay with cash</p>
+                            </label><br />
+                            
+                            <input type="radio" id="withinLagos" name="withinLagos" value="Within Lagos" />
+                            <label for="withinLagos">Within Lagos usually takes 4 to 5 working days
+                            <p>Delivery is free</p>
+                            </label><br />
+                            
+                            <input type="radio" id="park" name="park" value="Park pickup" />
+                            <label for="park">Outside Lagos Park pick up usually takes 5 to 6 working days
+                                <p>Pay with cash at the park</p>
+                            </label><br />
+                        </form>
+                        <button className='back-btn' onClick={() => handleClick('Shipping')}>
+                            Back
+                        </button>
+                        <button className='makePayment-btn' onClick={() => handleClick('Payment')}>Continue to payment</button>
+                        </div>
+                    )}
+
+
                     {activeLink === 'Payment' && (
                         <div>
                     <form className='payment-form'>
                         <h2>Payment Method</h2>
-                        <input type="radio" id="delivery" name="payment_method" value="Pay on delivery" />
-                        <label for="delivery">Pay on Delivery
-                            <p>Pay with cash on delivery</p>
+                        <input type="radio" id="delivery" name="payment_method" value="Pay online" />
+                        <label for="delivery">Paystack
+                            <p>Pay online using your Visa/Mastercard</p>
                         </label><br />
                         
-                        <input type="radio" id="credit" name="payment_method" value="Credit/Debit card" />
+                        {/* <input type="radio" id="credit" name="payment_method" value="Credit/Debit card" />
                         <label for="credit">Credit / Debit card
                             <p>Pay with your Credit / Debit Card</p>
                         </label><br />
@@ -111,59 +213,11 @@ const Payment = () => {
                         <input type="radio" id="others" name="payment_method" value="Other Payment Method" />
                         <label for="others">Other payment method
                             <p>Make payment through Gpay, Paypal, Paytm etc</p>
-                        </label><br />
+                        </label><br /> */}
                     </form>
-                    <button className='back-btn'>Back</button>
+                    <button className='back-btn' onClick={() => handleClick('Delivery')}>Back</button>
                     <button className='makePayment-btn'>Pay N{totalDue}</button>
                     </div>
-                    )}
-
-                    {activeLink === 'Shipping' && (
-                        <div>
-                            <form>
-                                <div>
-                                    <h2>Contact Details</h2>
-                                    <label htmlFor='firstname'>Firstname</label>
-                                    <input type='text' name='firstname' />
-
-                                    <label htmlFor='lastname'>Lastname:</label>
-                                    <input type='text' name='lastname' /><br />
-
-                                    <label htmlFor='email'>Email:</label>
-                                    <input type='email' name='email' /><br />
-
-                                    <label htmlFor='number'>Phone number:</label>
-                                    <input type='number' name='number' /><br />
-                                </div>
-
-                                <div>
-                                    <h2>Shipping Details</h2>
-                                    <label htmlFor='addressNo'>Flat/House no.</label>
-                                    <input type='number' name='addressNo' /><br />
-
-                                    <label htmlFor='address'>Address</label>
-                                    <input type='text' name='address' /><br/>
-
-                                    <label htmlFor='city'>City</label>
-                                    <input type='text' name='city' />
-
-                                    <label htmlFor='state'>State</label>
-                                    <input type='text' name='state' />
-
-                                    <label htmlFor='code'>Postal Code</label>
-                                    <input type='number' name='code' />
-
-                                    <label htmlFor='landmark'>Famous Landmark</label>
-                                    <input type='text' name='landmark' />
-
-                                    <input type='check' name='check' />
-                                    <label htmlFor='check'>My shipping and Billing address are the same</label>
-                                </div>
-                                <button type='submit'>Continue</button>
-                            </form>
-
-                            
-                        </div>
                     )}
 
                 </div>
