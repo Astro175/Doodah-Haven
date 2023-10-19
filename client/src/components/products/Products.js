c2ea39a2ee839ba6582ca9972108b0f3906d7f78
Nnenna Udefi <miriamudefi@gmail.com>
2023-10-19 22:06:04 +0100

    products-

--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -38,7 +38,7 @@ const Products = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4000/api/products/add')
+    fetch('http://localhost:4000/api/products')
       .then((response) => response.json())
       .then((data) => setProducts(data))
       .catch((error) => console.error('Error fetching products', error));
