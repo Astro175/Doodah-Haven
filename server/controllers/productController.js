53cb7747f4c24412815864df3e8a26f1b27ad831
astro175 <aspark400@gmail.com>
2023-10-22 00:21:20 +0100

    working on retreiving all products, response seems so slow, testing the getAll product route, made some changes in it

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -13,7 +13,6 @@ const getAllProducts = async (req, res) => {
     res.status(200).send({
       success: true,
       counTotal: products.length,
-      message: "ALlProducts ",
       products,
     });
   } catch (error) {
