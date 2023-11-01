37d115670a25e5fceee73712b1c3777ff6d9da76
astro175 <aspark400@gmail.com>
2023-11-01 07:26:06 +0100

    fixed the product update and delete route

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -164,7 +164,7 @@ const updateProduct = async (req, res) => {
 
 // Delete a product (admin-only): DELETE /api/products/delete/:id
 const deleteProduct = async (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
   // Checks if ID is valid
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: 'Invalid ID' });
